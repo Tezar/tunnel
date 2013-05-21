7d36231f787bb3796ad1c5bf9b34b297d5a59be5
Ales Tomecek <TomecekAles@seznam.cz>
2013-05-21 16:07:18 +0200

    prulet na W

--- Tunnel/main.cpp
+++ Tunnel/main.cpp
@@ -8,24 +8,58 @@ using namespace video;
 using namespace io;
 using namespace gui;
 
+
+class EventHandler : public IEventReceiver
+{
+public:
+    // This is the one method that we have to implement
+    virtual bool OnEvent(const SEvent& event)
+    {
+        // Remember whether each key is down or up
+        if (event.EventType == irr::EET_KEY_INPUT_EVENT){
+            KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+			printf("%i - > %i %i\n",event.KeyInput.Key,event.KeyInput.PressedDown, IsKeyDown(irr::KEY_KEY_W) );				
+		}
+
+        return false;
+    }
+
+    // This is used to check whether a key is being held down
+    virtual bool IsKeyDown(EKEY_CODE keyCode) const
+    {
+        return KeyIsDown[keyCode];
+    }
+    
+    EventHandler()
+    {
+        for (u32 i=0; i<KEY_KEY_CODES_COUNT; ++i)
+            KeyIsDown[i] = false;
+    }
+
+private:
+    // We use this array to store the current state of each key
+    bool KeyIsDown[KEY_KEY_CODES_COUNT];
+};
+
+
 int main()
 {
+	// create device
+    EventHandler receiver;
+
 	 IrrlichtDevice *device =
-		 createDevice( video::EDT_OPENGL, dimension2d<u32>(640, 480), 16,
-            false, false, false, 0);
+		 createDevice( video::EDT_OPENGL, dimension2d<u32>(800, 600), 16,
+            false, false, false, &receiver);
 
     if (!device)
         return 1;
-    device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
+
+	device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
 
 
     IVideoDriver* driver = device->getVideoDriver();
     ISceneManager* smgr = device->getSceneManager();
     IGUIEnvironment* guienv = device->getGUIEnvironment();
-
-
-    guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
-        rect<s32>(10,10,260,22), true);
 
     IAnimatedMesh* mesh = smgr->getMesh("media/sydney.md2");
     if (!mesh)
@@ -42,11 +76,22 @@ int main()
         node->setMaterialTexture( 0, driver->getTexture("media/sydney.bmp") );
     }
 
-    smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
+	ICameraSceneNode* camera = smgr->addCameraSceneNode(0, vector3df(0,0,-40), vector3df(0,0,0));
+
 
     while(device->run())
     {
         driver->beginScene(true, true, SColor(255,100,101,140));
+		
+		if(receiver.IsKeyDown(irr::KEY_KEY_W)){
+			vector3df pos = camera->getPosition();
+			camera->setPosition( pos+vector3df(0,0,0.1)  );
+			camera->setTarget( pos+vector3df(0,0,5) );
+		}
+            
+
+
+
 
         smgr->drawAll();
         guienv->drawAll();
