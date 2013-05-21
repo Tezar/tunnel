547f7a6142a299d5e90ec81e55f6d6881f516baa
Ales Tomecek <TomecekAles@seznam.cz>
2013-05-21 16:16:51 +0200

    objekty

--- Tunnel/main.cpp
+++ Tunnel/main.cpp
@@ -1,4 +1,6 @@
 #include <irrlicht.h>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 
 using namespace irr;
 
@@ -8,6 +10,8 @@ using namespace video;
 using namespace io;
 using namespace gui;
 
+
+#define MAX_OBJECTS 50
 
 class EventHandler : public IEventReceiver
 {
@@ -47,6 +51,8 @@ int main()
 	// create device
     EventHandler receiver;
 
+	ISceneNode* objects [MAX_OBJECTS];
+
 	 IrrlichtDevice *device =
 		 createDevice( video::EDT_OPENGL, dimension2d<u32>(800, 600), 16,
             false, false, false, &receiver);
@@ -79,15 +85,37 @@ int main()
 	ICameraSceneNode* camera = smgr->addCameraSceneNode(0, vector3df(0,0,-40), vector3df(0,0,0));
 
 
+	//naplníme tunel pøekážkama
+
+	 srand (time(NULL));
+	/* generate secret number between 1 and 10: */
+	
+	for(int i = 0; i < MAX_OBJECTS; i++){
+		objects[i] = smgr->addCubeSceneNode(2);
+		objects[i]->setPosition( vector3df( (rand() % 30) - 5, (rand() % 30) - 5, rand() % 80) );
+	}
+
+
+
     while(device->run())
     {
         driver->beginScene(true, true, SColor(255,100,101,140));
 		
+		vector3df pos = camera->getPosition();
+
 		if(receiver.IsKeyDown(irr::KEY_KEY_W)){
-			vector3df pos = camera->getPosition();
 			camera->setPosition( pos+vector3df(0,0,0.1)  );
 			camera->setTarget( pos+vector3df(0,0,5) );
 		}
+
+		for(int i = 0; i < MAX_OBJECTS; i++){
+			vector3df tmpPos = objects[i]->getPosition();
+			if(tmpPos.Z > pos.Z) continue;
+			
+			objects[i]->setPosition( vector3df( (rand() % 30) - 15, (rand() % 30) - 15, rand() % 80 + pos.Z) );
+		}
+
+		
             
 
 
