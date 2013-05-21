4f045c27bdce13123715e3638e767c8ed6cfa429
Ales Tomecek <TomecekAles@seznam.cz>
2013-05-21 22:02:00 +0200

    pouziti rendereru

--- Tunnel/main.cpp
+++ Tunnel/main.cpp
@@ -1,6 +1,7 @@
 #include <irrlicht.h>
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
+#include "HDMStereoRender/HMDStereoRender.h" 
 
 using namespace irr;
 
@@ -20,38 +21,26 @@ float VIEWPORT_OFFSET = 5.0;
 class EventHandler : public IEventReceiver
 {
 public:
-    // This is the one method that we have to implement
-    virtual bool OnEvent(const SEvent& event)
-    {
-        // Remember whether each key is down or up
-        if (event.EventType == irr::EET_KEY_INPUT_EVENT){
-            KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
-			printf("%i - > %i %i\n",event.KeyInput.Key,event.KeyInput.PressedDown, IsKeyDown(irr::KEY_KEY_W) );				
-		}
+   bool OnEvent(const SEvent& event)
+   {   
+      if (event.EventType == EET_KEY_INPUT_EVENT && event.KeyInput.PressedDown) {
+        if (event.KeyInput.Key == KEY_ESCAPE)
+        {
+          device->closeDevice();
+          return true;
+        }
+      }
+      return false;
+   }
+   IrrlichtDevice* device;
 
-        return false;
-    }
-
-    // This is used to check whether a key is being held down
-    virtual bool IsKeyDown(EKEY_CODE keyCode) const
-    {
-        return KeyIsDown[keyCode];
-    }
-    
-    EventHandler()
-    {
-        for (u32 i=0; i<KEY_KEY_CODES_COUNT; ++i)
-            KeyIsDown[i] = false;
-    }
-
-private:
-    // We use this array to store the current state of each key
-    bool KeyIsDown[KEY_KEY_CODES_COUNT];
-};
+}; 
 
 
-const int ResX=1680;
-const int ResY=1050;
+const int ResX=1280;
+const int ResY=800;
+bool fullscreen = true;
+bool vsync = true;
 
 int main()
 {
@@ -60,45 +49,69 @@ int main()
 
 	ISceneNode* objects [MAX_OBJECTS];
 
-	ICameraSceneNode* leftEye;
-	ICameraSceneNode* rightEye;
 
 
-	 IrrlichtDevice *device =
-		 createDevice( video::EDT_OPENGL, dimension2d<u32>(ResX, ResY), 16,
-            true, false, false, &receiver);
+	 IrrlichtDevice *device = createDevice(EDT_OPENGL, dimension2d<u32>(ResX, ResY), 16, fullscreen, false, vsync, &receiver); 
+	 
+	 receiver.device = device; 
 
     if (!device)
         return 1;
-
-	device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
-
 
     IVideoDriver* driver = device->getVideoDriver();
     ISceneManager* smgr = device->getSceneManager();
     IGUIEnvironment* guienv = device->getGUIEnvironment();
 
-    IAnimatedMesh* mesh = smgr->getMesh("media/sydney.md2");
-    if (!mesh)
-    {
-        device->drop();
-        return 1;
-    }
-    IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode( mesh );
+	HMDDescriptor HMD;
+	// Parameters from the Oculus Rift DK1
+	HMD.hResolution = ResX;
+	HMD.vResolution = ResY;
+	HMD.hScreenSize = 0.14976;
+	HMD.vScreenSize = 0.0936;
+	HMD.interpupillaryDistance = 0.064;
+	HMD.lensSeparationDistance = 0.064;
+	HMD.eyeToScreenDistance = 0.041;
+	HMD.distortionK[0] = 1.0;
+	HMD.distortionK[1] = 0.22;
+	HMD.distortionK[2] = 0.24;
+	HMD.distortionK[3] = 0.0;
 
+	HMDStereoRender renderer(device, HMD, 10); 
+
+	smgr->addCameraSceneNodeFPS();
+
+	 device->getCursorControl()->setVisible(false); 
+
+
+	// load the quake map
+	device->getFileSystem()->addZipFileArchive("media/map-20kdm2.pk3");
+	IAnimatedMesh* mesh = smgr->getMesh("20kdm2.bsp");
+	ISceneNode* levelNode = smgr->addOctreeSceneNode(mesh->getMesh(0), 0, -1, 128);
+	levelNode->setPosition(core::vector3df(-1350,-90,-1400));
+	  
+	// load a faerie 
+	IAnimatedMesh* faerie = smgr->getMesh("media/faerie.md2");
+	IAnimatedMeshSceneNode* faerieNode = smgr->addAnimatedMeshSceneNode(faerie);
+	faerieNode->setMaterialTexture(0, driver->getTexture("media/faerie2.bmp"));
+	faerieNode->setMaterialFlag(EMF_LIGHTING, false);
+	faerieNode->setPosition(vector3df(40,190,-1030));
+	faerieNode->setRotation(vector3df(0,-90,0));
+	faerieNode->setMD2Animation(EMAT_SALUTE);
+
+	// load a dwarf
+	IAnimatedMesh* dwarf = smgr->getMesh("media/dwarf.x");
+	IAnimatedMeshSceneNode* dwarfNode = smgr->addAnimatedMeshSceneNode(dwarf);
+	dwarfNode->setPosition(vector3df(40,-25,20));
+	  
+
+	/*  
     if (node)
     {
         node->setMaterialFlag(EMF_LIGHTING, false);
         node->setMD2Animation(scene::EMAT_STAND);
         node->setMaterialTexture( 0, driver->getTexture("media/sydney.bmp") );
     }
-
-
-	ISceneNode* cameraRig = smgr->addEmptySceneNode();
-	leftEye = smgr->addCameraSceneNode(cameraRig);
-	leftEye->bindTargetAndRotation(false);
-	rightEye = smgr->addCameraSceneNode(cameraRig);
-	
+	*/
 
 
 	vector3df pos = vector3df(0,0,0);
@@ -113,57 +126,22 @@ int main()
 		objects[i]->setPosition( vector3df( (rand() % 30) - 5, (rand() % 30) - 5, rand() % 80) );
 	}
 
+	device->setInputReceivingSceneManager(smgr);
 	
     while(device->run())
     {
         driver->beginScene(true, true, SColor(255,100,101,140));
 		
-		if(receiver.IsKeyDown(irr::KEY_KEY_W)){
-			pos.Z += 0.1;
-			cameraRig->setPosition(pos);
-		}
-
-		if(receiver.IsKeyDown(irr::KEY_KEY_A)){
-			EYES_OFFSET += 0.01;
-			printf("eye %3.2f\n",EYES_OFFSET);
-		}
-
-		if(receiver.IsKeyDown(irr::KEY_KEY_D)){
-			EYES_OFFSET -= 0.01;
-			printf("eye %3.2f\n",EYES_OFFSET);
-		}
-
-		if(receiver.IsKeyDown(irr::KEY_KEY_Q)){
-			VIEWPORT_OFFSET += 0.1;
-			printf("view %3.2f\n",VIEWPORT_OFFSET);
-		}
-
-		if(receiver.IsKeyDown(irr::KEY_KEY_E)){
-			VIEWPORT_OFFSET -= 0.1;
-			printf("view %3.2f\n",VIEWPORT_OFFSET);
-		}
-
 		for(int i = 0; i < MAX_OBJECTS; i++){
 			vector3df tmpPos = objects[i]->getPosition();
 			if(tmpPos.Z > pos.Z) continue;
 			
 			objects[i]->setPosition( vector3df( (rand() % 30) - 15, (rand() % 30) - 15, rand() % 80 + pos.Z) );
 		}
-
-		leftEye->setPosition( vector3df(-EYES_OFFSET,5,0));
-		rightEye->setPosition( vector3df(EYES_OFFSET,5,0));
 		
-
-		smgr->setActiveCamera(leftEye);
-		leftEye->setTarget( pos + vector3df(-EYES_OFFSET,5,5));
-		driver->setViewPort(rect<s32>(-VIEWPORT_OFFSET,0,ResX/2-VIEWPORT_OFFSET,ResY));
-        smgr->drawAll();
-
-		smgr->setActiveCamera(rightEye);
-		rightEye->setTarget( pos + vector3df(+EYES_OFFSET,5,5));
-		driver->setViewPort(rect<s32>(ResX/2+VIEWPORT_OFFSET,0,ResX+VIEWPORT_OFFSET,ResY));
-        smgr->drawAll();
-
+		renderer.drawAll(smgr); 
+		//smgr->drawAll();
+        
         guienv->drawAll();
 
         driver->endScene();
